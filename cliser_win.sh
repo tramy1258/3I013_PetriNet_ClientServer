01d2a6df555ba56f71ba9b2f43f78c70ba720012
Tra My Nguyen <tramynvt@gmail.com>
2020-04-06 19:34:21 +0700

    scripts fixed

--- cliser_win.sh
+++ cliser_win.sh
@@ -6,12 +6,12 @@ then
 	echo "Must have at least one client and one server";
 else
 	java -classpath ".\bin\;\
-	.\lib\fr.lip6.pnml.framework.3rdpartimports_2.2.14.jar;\
-	.\lib\fr.lip6.pnml.framework.ptnet_2.2.14.jar;\
-	.\lib\fr.lip6.pnml.framework.utils_2.2.14.jar;\
-	.\lib\org.eclipse.emf.common_2.17.0.v20190920-0401.jar;\
-	.\lib\org.eclipse.emf.ecore_2.20.0.v20190920-0401.jar;\
-	.\lib\org.slf4j.api_1.7.30.v20200204-2150.jar;\
-	.\lib\slf4j-jdk14-2.0.0-alpha1.jar" clientserver.TestGenerator $1 $2;
+.\lib\fr.lip6.pnml.framework.3rdpartimports_2.2.14.jar;\
+.\lib\fr.lip6.pnml.framework.ptnet_2.2.14.jar;\
+.\lib\fr.lip6.pnml.framework.utils_2.2.14.jar;\
+.\lib\org.eclipse.emf.common_2.17.0.v20190920-0401.jar;\
+.\lib\org.eclipse.emf.ecore_2.20.0.v20190920-0401.jar;\
+.\lib\org.slf4j.api_1.7.30.v20200204-2150.jar;\
+.\lib\slf4j-jdk14-2.0.0-alpha1.jar" clientserver.TestGenerator $1 $2;
 fi
 #logback-classic-1.2.3.jar
